dbb4ea9622f386abb7abb08b61e06929c8051fb4
Arngrimur Dagur <111790903+ArngrimurDagur@users.noreply.github.com>
2025-05-12 14:58:53 +0000

    Update InstallBrowsersFix.bat

--- InstallBrowsersFix.bat
+++ InstallBrowsersFix.bat
@@ -3,7 +3,7 @@ echo Installing Firefox...
 start /wait \\DC01\Installers\FirefoxSetup.exe /silent
 
 echo Installing Chrome...
-start /wait \\DC01\Installers\ChromeStandaloneSetup64.exe /silent /install
+start /wait \\DC01\Installers\ChromeSetup.exe /silent /install
 
 echo Installation complete.
 exit
